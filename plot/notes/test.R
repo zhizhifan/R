25d594a3c8c7b4a41567aae61b5d29204e4ac519
zhizhifan <2120212517@stu.nufe.edu.cn>
2023-12-16 23:21:38 +0800

    changes

--- plot/notes/test.R
+++ plot/notes/test.R
@@ -1,5 +1,18 @@
+# library(tidyverse)
+# d <- ggplot(mtcars, aes(cyl, mpg)) + geom_point()
+# d + stat_summary(fun.data = "mean_cl_boot", colour = "red", linewidth = 2, size = 3)
+# # You can supply individual functions to summarise the value at
+# # each x:
+# d + stat_summary(fun = "median", colour = "red", size = 2, geom = "point")
+# # d + stat_summary(fun = "mean", colour = "red", size = 2, geom = "point")
+# # d + aes(colour = factor(vs)) + stat_summary(fun = mean, geom="line")
+# Orientation follows the discrete axis
+# ggplot(mtcars, aes(mpg, factor(cyl))) +
+#   geom_point() +
+#   stat_summary(fun.data = "mean_cl_boot", colour = "red", linewidth = 2, size = 3)
+
 library(tidyverse)
-mpg %>% 
-  ggplot(aes(x = cty)) +
-  geom_histogram(aes(y = after_stat(density)), fill = "steelblue") +
+ggplot(mtcars, aes(cyl, mpg)) +
+  geom_point() +
+  stat_summary(fun = "mean", color = "#007c7c", size = 4, geom = "point") +
   theme_minimal()
