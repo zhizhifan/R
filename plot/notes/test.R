6c8d51a72671ef570ac6c7c2a756e8e15991c87f
zhizhifan <2120212517@stu.nufe.edu.cn>
2023-12-19 18:26:49 +0800

    change

--- plot/notes/test.R
+++ plot/notes/test.R
@@ -26,16 +26,107 @@
 #   stat_bin(bins = 3, geom = "bar") +
 #   theme_minimal()
 
+# library(tidyverse)
+# library(patchwork)
+# p1 <- ggplot(diamonds, aes(x = carat)) +
+#   scale_x_continuous(limits = c(0, 3)) +
+#   stat_bin(aes(y = after_stat(density)), binwidth = .1, geom = "point") +
+#   theme_minimal()
+
+# p2 <- ggplot(diamonds, aes(x = carat)) +
+#   stat_bin(aes(y = after_stat(density)), binwidth = .1, geom = "point", color = "red") +
+#   scale_x_continuous(limits = c(0, 3)) +
+#   theme_minimal()
+
+# p1 / p2
+
+# library(tidyverse)
+# library(patchwork)
+# data(diamonds)
+
+# p1 <- ggplot(diamonds, aes(log10(carat), price)) +
+#   geom_point() +
+#   theme_minimal()
+
+# p2 <- ggplot(diamonds, aes(carat, price)) +
+#   geom_point() +
+#   scale_x_log10() +
+#   theme_minimal()
+
+# p1 / p2
+
+# Equivalent fill scales do the same job for the fill aesthetic
+# ggplot(mtcars, aes(mpg, disp)) +
+#   geom_point(aes(color = disp), size = 3) +
+#   scale_color_gradient2(mid = "#008c8c", midpoint = mean(mtcars$disp)) +
+#   theme_minimal()
+
+
 library(tidyverse)
 library(patchwork)
-p1 <- ggplot(diamonds, aes(x = carat)) +
-  scale_x_continuous(limits = c(0, 3)) +
-  stat_bin(aes(y = after_stat(density)), binwidth = .1, geom = "point") +
+# p1 <- ggplot(mtcars, aes(mpg, disp)) +
+#   geom_point(aes(color = disp), size = 3) +
+#   scale_color_gradientn(
+#     colours = viridis::viridis_pal()(2), 
+#     breaks = scales::breaks_width(50)
+#     # guide = "colorsteps"
+#   ) +
+#   theme_minimal()
+# set.seed(123)
+# test_df <- tibble(
+#   id = 1:1000,
+#   value = rnorm(1000)
+# )
+
+# p2 <- ggplot(test_df, aes(id, value)) +
+#   geom_point(aes(color = value), size = 3) +
+#   scale_color_gradientn(
+#     colours = viridis::viridis_pal()(2), 
+#     values = scales::rescale(mtcars$disp, to = c(0, 1)), 
+#     # guide = "colorsteps"
+#   ) +
+#   theme_minimal()
+
+# # p1 / p2
+# print(p2)
+
+# library(ggplot2)
+# library(scales) # for rescale function
+# library(viridis) # for viridis color palette
+
+# # Load the mtcars dataset
+# data(mtcars)
+
+# # Rescale the 'disp' values to a range between 0 and 1
+# disp_rescaled <- rescale(mtcars$disp)
+
+# # Create a more impactful color scale
+# # We'll create a non-linear transformation for the 'values' parameter
+# # that emphasizes the dense regions
+# values <- (disp_rescaled)^2 # Squaring to emphasize lower values more
+
+# ggplot(mtcars, aes(mpg, disp)) +
+#   geom_point(aes(color = disp), size = 3) +
+#   scale_color_gradientn(
+#     colours = viridis_pal()(5),
+#     values = values, 
+#     # guide = "coloursteps"
+#   ) +
+#   theme_minimal() +
+#   labs(color = "Displacement (cc)")
+
+
+# 创建示例数据
+set.seed(123)
+data <- data.frame(
+  Temperature = seq(-10, 30, by = 0.5),
+  Measurement = rnorm(81, mean = 10, sd = 5)
+)
+ggplot(data, aes(x = Temperature, y = Measurement)) +
+  geom_point(aes(color = Temperature), size = 3) +
+  scale_colour_gradientn(
+    colors = c("blue", "white", "orange", "red"),
+    values = scales::rescale(c(-10, 0, 20, 30))
+  ) +
   theme_minimal()
 
-p2 <- ggplot(diamonds, aes(x = carat)) +
-  stat_bin(aes(y = after_stat(density)), binwidth = .1, geom = "point", color = "red") +
-  scale_x_continuous(limits = c(0, 3)) +
-  theme_minimal()
-
-p1 / p2
